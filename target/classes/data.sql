baef8d123d63db69a578f377ac51b81035cf0452
Jerm0nt <s0566974@htw-berlin.de>
2022-07-17 17:35:24 +0200

    Minor fixes

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO person (id, name, last_name, zip_code, city, color_id)
+VALUES
+  (1, 'Hans', 'Müller', 67742, 'Lauterecken', 1),
+  (2, 'Peter', 'Petersen', 18439, 'Stralsund', 2),
+  (3, 'Johnny', 'Johnson', 88888 , 'made up', 3),
+  (4, 'Milly', 'Millenium', 77777, 'made up too', 4),
+  (5, 'Jonas', 'Müller', 32323, 'Hansstadt', 5),
+  (6, 'Tastatur', 'Fujitsu', 42342, 'Japan', 6),
+  (7, 'Anders', 'Andersson', 32132, 'Schweden- ☀', 2),
+  (10, 'Gerda', 'Gerber', 76535, 'Woanders', 3),
+  (11, 'Klaus', 'Klaussen', 43246, 'Hierach', 2);
